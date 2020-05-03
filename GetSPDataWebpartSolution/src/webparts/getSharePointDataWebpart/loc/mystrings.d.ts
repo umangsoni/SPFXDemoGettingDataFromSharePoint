e3973790bcc0718b777f4d2a39fd59c796140436
Umang Soni <umang.soni2011@gmail.com>
2020-05-03 12:31:23 +0530

    Webpart created

---
+++ GetSPDataWebpartSolution/src/webparts/getSharePointDataWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGetSharePointDataWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GetSharePointDataWebpartWebPartStrings' {
+  const strings: IGetSharePointDataWebpartWebPartStrings;
+  export = strings;
+}
